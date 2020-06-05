9886b88e688365606d02846996d4c14bce609e29
Dan Spratling <daniel.spratling@toumetis.com>
2020-06-05 23:18:16 +0100

    Starter using svelte and tailwindcss

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let name
+</script>
+
+<main>
+  <h1 class="font-bold text-4xl text-green-400 background-blue-300">
+    Hi {name}!
+  </h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
